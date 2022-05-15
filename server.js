edc05f7821901e5c49701d766e0c3df8881d7958
JaquelineAvila1 <avilayaky33@gmail.com>
2022-05-15 00:12:45 -0600

    Agregando funciones y dependencias

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const multer = require('multer');
+const mimeTypes = require('mime-types');
+
+//funcion para que se reconozcan diferentes extensiones de archivos
+
+const storage = multer.diskStorage({
+    destination: 'uploads/', 
+    filename: function(req, file, cb){
+        cb("",Date.now() + "." + mimeTypes.extension(file.mimetype));
+    }
+})
+
+
+
+//funcion que leera los archivos
+const upload = multer({
+    storage: storage
+})
+
+app.get("/",(req, res)=> {
+    console.log(__dirname)
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+app.post("/files", upload.single('nombre'), (req,res)=>{
+    res.send("Archivo guardado exitosamente");
+
+})
+
+app.listen(8080,()=>console.log("Servidor iniciado"))
